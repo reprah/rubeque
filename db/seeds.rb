0e7bd7d5618fff7ec938f65c420c43e073d3feae
Max Holder <max.holder@scimedsolutions.com>
2013-09-11 17:34:35 -0400

    Fix code for Blackjack problem to check for values under 21
    Fixes #92

--- db/seeds.rb
+++ db/seeds.rb
@@ -140,6 +140,7 @@ ___
 assert_equal twenty_one?(3, 4, 5, 6, 3), true
 assert_equal twenty_one?(3, 11, 10), false
 assert_equal twenty_one?(10, 11), true
+assert_equal twenty_one?(10, 10), false
 code_block
                )
 
